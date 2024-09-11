ef5d57d8b2488822aa0e9fc0164d6ed27e0581f3
ejim favour <favourejim56@gmail.com>
2024-09-11 05:45:01 +0100

    created the data for the hashtags

---
+++ lib/data/hashtags.dart
@@ -0,0 +1,22 @@
+import 'package:xwager/models/hashtag.dart';
+
+final List<Hashtag> hashtags = [
+  Hashtag(hashtag: Hashtags.esports, title: 'Esports'),
+  Hashtag(hashtag: Hashtags.football, title: 'Football'),
+  Hashtag(hashtag: Hashtags.hockey, title: 'Hockey'),
+  Hashtag(hashtag: Hashtags.golf, title: 'Golf'),
+  Hashtag(hashtag: Hashtags.ufc, title: 'UFC'),
+  Hashtag(hashtag: Hashtags.boxing, title: 'Boxing'),
+  Hashtag(hashtag: Hashtags.cricket, title: 'Cricket'),
+  Hashtag(hashtag: Hashtags.snooker, title: 'Snooker'),
+  Hashtag(hashtag: Hashtags.bowls, title: 'Bowls'),
+  Hashtag(hashtag: Hashtags.tennis, title: 'Tennis'),
+  Hashtag(hashtag: Hashtags.drafts, title: 'Drafts'),
+  Hashtag(hashtag: Hashtags.rugby, title: 'Rugby'),
+  Hashtag(hashtag: Hashtags.racing, title: 'Racing'),
+  Hashtag(hashtag: Hashtags.handball, title: 'Handball'),
+  Hashtag(hashtag: Hashtags.baseball, title: 'Baseball'),
+  Hashtag(hashtag: Hashtags.volleyball, title: 'Volleyball'),
+  Hashtag(hashtag: Hashtags.fantasyLeague, title: 'FantasyLeague'),
+  Hashtag(hashtag: Hashtags.preseason, title: 'Preseason')
+];
